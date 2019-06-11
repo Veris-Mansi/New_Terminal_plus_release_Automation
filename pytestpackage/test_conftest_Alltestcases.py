39af51d94c7f98834320882108516f91b7291016
Mansi Sahu <mansi.sahu@veris.in>
2019-06-11 18:56:09 +0530

    all online cases of mobile and email including general activity and invites

--- pytestpackage/test_conftest_Alltestcases.py
+++ pytestpackage/test_conftest_Alltestcases.py
@@ -36,6 +36,7 @@ class TestWalk_In():
 
     def test_fresh_user_walkin(self):
         walkin_visitor(self.driver,self.walkin_details)
+        self.driver.background_app(1)
 
     def test_member_check_in(self):
         try:
@@ -45,44 +46,40 @@ class TestWalk_In():
             time.sleep(0.5)
             #setting_contact_member(self.driver)
             setting_contact_member_touch(self.driver)
-            print("old screen")
             phone = WebDriverWait(self.driver, 10, poll_frequency=0.005).until(
                 EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Employee')))
             phone.click()
 
             cameraretake(self.driver)
-            FLEP_auto_fetch_member(self.driver, self.member_details[0])
+            print(type(self.member_details))
+            print(self.member_details)
+            FLEP_auto_fetch_member(self.driver, self.member_details)
 
             Meeting_with_screen(self.driver)
             time.sleep(1)
             try:
-                emergency_details_autofetch(self.driver, self.member_details[0])
-                unique_id_autofetch(self.driver, self.member_details[0]['unique_id'])
+                emergency_details_autofetch(self.driver, self.member_details)
+                unique_id_autofetch(self.driver, self.member_details['unique_id'])
 
             except:
-                emergency_contact(self.driver,self.member_details[0])
-                unique_id(self.driver,self.member_details[0])
+                emergency_contact(self.driver,self.member_details)
+                unique_id(self.driver,self.member_details)
 
             Next(self.driver)
             time.sleep(1)
-            print(type(self.member_details))
-            print(self.member_details)
-            print(type(self.member_details[0]))
-            print(self.member_details[0])
-            activity_complete(self.driver, self.member_details[0])
+            activity_complete(self.driver, self.member_details)
             self.status_test = True
             statusOftest(self.status_test, self.driver)
-            self.driver.background_app(2)
+
         except:
             self.status_test = False
             takeScreenshot(self.driver)
             statusOftest(self.status_test, self.driver)
             raise
-
     def test_invited_user(self):
         try:
             checkIn(self.driver)
-            contact=setting_contact_invite(self.driver)
+            contact=setting_invite_touch(self.driver)
             InvitedWF(self.driver)
             FLEP_auto_fetch_visitor(self.driver,self.invited_details,contact)
             meeting_with_invite(self.driver)
@@ -91,7 +88,7 @@ class TestWalk_In():
             activity_complete(self.driver, self.invited_details)
             self.status_test = True
             statusOftest(self.status_test, self.driver)
-            #self.driver.background_app(2)
+            self.driver.background_app(2)
         except:
             self.status_test = False
             takeScreenshot(self.driver)
@@ -109,16 +106,16 @@ class TestWalk_In():
             cameraretake(self.driver)
             FLEP_auto_fetch_member(self.driver, self.member_details)
             time.sleep(0.5)
-            emergency_details_autofetch(self.driver, self.member_details[0])
+            emergency_details_autofetch(self.driver, self.member_details)
             time.sleep(0.5)
             Next(self.driver)
-            unique_id_autofetch(self.driver, self.member_details[0]['unique_id'])
+            unique_id_autofetch(self.driver, self.member_details['unique_id'])
             gender_Screen(self.driver)
             Next(self.driver)
-            activity_complete_general(self.driver, self.member_details[0])
+            activity_complete_general(self.driver, self.member_details)
             self.status_test = True
             statusOftest(self.status_test, self.driver)
-            self.driver.background_app(2)
+            #self.driver.background_app(2)
             assert True
         except:
             print("exception")
@@ -126,7 +123,6 @@ class TestWalk_In():
             self.status_test = False
             statusOftest(self.status_test, self.driver)
             raise
-
 
     def test_general_activity_walkin(self):
 
@@ -146,28 +142,120 @@ class TestWalk_In():
             activity_complete_general(self.driver, self.walkin_details)
             self.status_test = True
             statusOftest(self.status_test, self.driver)
-            #self.driver.background_app(2)
-        except:
-            print("exception")
-            takeScreenshot(self.driver)
-
-            self.status_test = False
-            statusOftest(self.status_test, self.driver)
-            raise
-    def test_email_walkin(self):
-        try:
-
-            time.sleep(1)
-            walkin_visitor(self.driver, self.walkin_email_details)
             self.driver.background_app(2)
         except:
             print("exception")
             takeScreenshot(self.driver)
+
             self.status_test = False
             statusOftest(self.status_test, self.driver)
             raise
 
+    def test_email_walkin(self):
+        try:
 
+            time.sleep(1)
+            # walkin_visitor(self.driver, self.walkin_email_details)
+            checkIn(self.driver)
+            time.sleep(0.5)
+            email = setting_email(self.driver)
+            wf = WebDriverWait(self.driver, 10, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Visitor2_email')))
+            wf.click()
+            camera(self.driver)
+            m = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Emergencycontactname')))
+            m.send_keys(self.walkin_email_details['Emergency_contact_name'])
+            Next(self.driver)
+            FLEP_Email_Screen(self.driver, self.walkin_email_details, email)
+            activity_complete(self.driver, self.walkin_email_details)
+            self.driver.background_app(2)
+        except:
+            takeScreenshot(self.driver)
+            print("exception")
+            self.status_test = False
+            statusOftest(self.status_test, self.driver)
+            raise
+
+    def test_email_member(self):
+
+        try:
+            checkIn(self.driver)
+            time.sleep(0.5)
+
+            setting_email_member(self.driver)
+            phone = WebDriverWait(self.driver, 10, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Employee')))
+            phone.click()
+            print("sleep")
+            time.sleep(0.5)
+            cameraretake(self.driver)
+
+            print("sleep")
+            time.sleep(0.5)
+            FLEP_auto_fetch_member(self.driver, self.member_email_detail)
+            Next(self.driver)
+            Meeting_with_screen(self.driver)
+            emergency_details_autofetch(self.driver, self.member_email_detail)
+            unique_id_autofetch(self.driver, self.member_email_detail['unique_id'])
+            Next(self.driver)
+            time.sleep(1)
+            activity_complete(self.driver, self.member_email_detail)
+            self.status_test = True
+            statusOftest(self.status_test, self.driver)
+            self.driver.background_app(1)
+        except:
+            self.status_test = False
+            takeScreenshot(self.driver)
+            statusOftest(self.status_test, self.driver)
+            raise
+
+    def test_email_invited(self):
+        try:
+            checkIn(self.driver)
+            email = setting_email_invite(self.driver)
+            InvitedWF(self.driver)
+            FLEP_Autofetch_Email_walkin(self.driver, self.email_invited_details, email)
+            meeting_with_invite(self.driver)
+            Next(self.driver)
+            time.sleep(2)
+            activity_complete(self.driver, self.email_invited_details)
+            self.status_test = True
+            statusOftest(self.status_test, self.driver)
+        except:
+            self.status_test = False
+            takeScreenshot(self.driver)
+            statusOftest(self.status_test, self.driver)
+            raise
+
+    def test_general_activity_emailmember(self):
+        try:
+
+            time.sleep(0.5)
+            late_tracking(self.driver)
+            setting_email_member(self.driver)
+            Next(self.driver)
+            # time.sleep(2)
+            cameraretake(self.driver)
+            FLEP_auto_fetch_member(self.driver, self.member_email_detail)
+            time.sleep(0.5)
+            emergency_details_autofetch(self.driver, self.member_email_detail)
+            time.sleep(0.5)
+            Next(self.driver)
+            unique_id_autofetch(self.driver, self.member_email_detail['unique_id'])
+            gender_Screen(self.driver)
+            Next(self.driver)
+            activity_complete_general(self.driver, self.member_email_detail)
+            self.status_test = True
+            statusOftest(self.status_test, self.driver)
+            self.driver.background_app(1)
+            assert True
+        except:
+            print("exception")
+            takeScreenshot(self.driver)
+            self.status_test = False
+            statusOftest(self.status_test, self.driver)
+            raise
     def test_general_activity_walkin_autofetch(self):
         try:
             late_tracking(self.driver)
@@ -185,7 +273,7 @@ class TestWalk_In():
             # check_out(self.driver, self.walkin_details)
             self.status_test = True
             statusOftest(self.status_test, self.driver)
-            self.driver.background_app(2)
+            #self.driver.background_app(2)
         except:
             print("exception")
             takeScreenshot(self.driver)
@@ -193,19 +281,35 @@ class TestWalk_In():
             statusOftest(self.status_test, self.driver)
             raise
 
-    def test_autofetch_user_walkin(self):
-
-        autofetch_user(self.driver, self.walkin_details)
-
     def test_email_autofetch(self):
         try:
-            autofetch_user(self.driver,self.walkin_email_details)
-            self.driver.background_app(2)
+            # autofetch_user(self.driver,self.walkin_email_details)
+            checkIn(self.driver)
+            time.sleep(0.5)
+            email = setting_email(self.driver)
+            wf = WebDriverWait(self.driver, 10, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Visitor2_email')))
+            wf.click()
+            cameraretake(self.driver)
+
+            emer_name = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Emergencycontactname')))
+            name = emer_name.text
+            print(name)
+            assert name == self.walkin_email_details['Emergency_contact_name']
+            Next(self.driver)
+            FLEP_Autofetch_Email_walkin(self.driver, self.walkin_email_details, email)
+            activity_complete(self.driver, self.walkin_email_details)
+
         except:
+            takeScreenshot(self.driver)
             print("exception")
             self.status_test = False
             statusOftest(self.status_test, self.driver)
             raise
+    def test_autofetch_user_walkin(self):
+
+        autofetch_user(self.driver, self.walkin_details)
 
     def test_checkout_walkinuser(self):
        try:
@@ -218,8 +322,8 @@ class TestWalk_In():
            raise
     def test_checkout_member(self):
         try:
-            check_out(self.driver, self.member_details[0])
-            self.driver.background_app(1)
+            check_out(self.driver, self.member_details)
+            #self.driver.background_app(1)
         except:
             print("exception")
             self.status_test = False
@@ -234,5 +338,33 @@ class TestWalk_In():
             self.status_test = False
             statusOftest(self.status_test, self.driver)
             raise
+    def test_email_checkout_walkinuser(self):
+        try:
+            check_out(self.driver, self.walkin_email_details)
 
+        except:
+            print("exception")
+            self.status_test = False
+            statusOftest(self.status_test, self.driver)
+            raise
+
+    def test_email_checkout_invite(self):
+        try:
+            check_out(self.driver, self.email_invited_details)
+
+        except:
+            print("exception")
+            self.status_test = False
+            statusOftest(self.status_test, self.driver)
+            raise
+
+    def test_email_checkout_member(self):
+        try:
+            check_out(self.driver, self.member_email_detail)
+
+        except:
+            print("exception")
+            self.status_test = False
+            statusOftest(self.status_test, self.driver)
+            raise
 
