909b198fd2acaddd3faf727fc607fbfc68583bd3
Mansi Sahu <mansi.sahu@veris.in>
2019-06-11 19:28:20 +0530

    basic test case for doing multiple checkin for a single number

--- pytestpackage/test_conftest_NDA.py
+++ pytestpackage/test_conftest_NDA.py
@@ -1,6 +1,6 @@
 import pytest
 import time
-from utilities.Resources2 import *
+from utilities.Resources_small import *
 
 @pytest.mark.usefixtures("data","driver")
 class TestWalk_In():
@@ -45,46 +45,76 @@ class TestWalk_In():
             EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'LOGIN')))
         r.click()
 
-        for i in range(5):
+        for i in range(2):
             m = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
-                EC.presence_of_element_located((By.XPATH, '//android.view.ViewGroup[@content-desc="Check-In"]')))
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Check-In')))
+            m.click()
+            print(type(self.member_details))
+            print(self.member_details)
+            setting_contact_touch(self.driver)
+
+            m = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'NDA_ONLY')))
+            m.click()
+            cameraretake(self.driver)
+
+            r = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Confirm Check-in')))
+            r.click()
+            self.driver.background_app(2)
+        for i in range(2):
+            m = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Check-In')))
             m.click()
 
-            for i in range(10):
-                m = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
-                    EC.presence_of_element_located((By.XPATH,
-                                                    '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[16]/android.view.ViewGroup')))
-                m.click()
+            setting_contact_member(self.driver)
+
+            m = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'NDA_ONLYtwo')))
+            m.click()
+            cameraretake(self.driver)
 
             r = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
-                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Next')))
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Confirm Check-in')))
             r.click()
+            print("executed " + str(i))
+            self.driver.close_app()
+            time.sleep(0.5)
+            self.driver.activate_app('com.veristerminal')
+            print(self.member_details)
+            print(type(self.member_details))
 
-            r = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+        for i in range(1):
+            m = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Check-In')))
+            m.click()
+
+            setting_contact(self.driver)
+
+            m = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
                 EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'NDA_ONLY')))
-            r.click()
-
-            time.sleep(2)
-
-            r = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
-                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'signatureField')))
-            r.click()
-
-            """r = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
-                EC.presence_of_element_located((By.XPATH,
-                                                '//android.view.ViewGroup[@content-desc="signatureField"]/android.widget.LinearLayout/android.view.View')))
-            r.click()"""
-            time.sleep(1)
-            user_action = TouchAction(self.driver)
-            user_action.press(x=221, y=901).move_to(x=393, y=911).release().perform()
-            time.sleep(1)
-
-            r = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
-                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Next')))
-            r.click()
+            m.click()
+            cameraretake(self.driver)
 
             r = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
                 EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Confirm Check-in')))
             r.click()
             print("executed "+str(i))
-            time.sleep(1)
+
+        for i in range(2):
+            m = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Check-In')))
+            m.click()
+
+            setting_contact_member(self.driver)
+
+            m = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'NDA_ONLYtwo')))
+            m.click()
+            cameraretake(self.driver)
+
+            r = WebDriverWait(self.driver, 20, poll_frequency=0.005).until(
+                EC.presence_of_element_located((By.ACCESSIBILITY_ID, 'Confirm Check-in')))
+            r.click()
+            print("executed " + str(i))
+
